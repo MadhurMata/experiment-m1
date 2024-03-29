8ad154b4f229a8c0ad5f826635a931a36e8af4bf
MadhurMataG <madhur.mata@appspace.com>
2024-03-29 09:36:48 +0100

    test

--- pages/Home.js
+++ pages/Home.js
@@ -6,7 +6,7 @@ function Home() {
 
 Home.prototype.generateElements = function() {
   this.pageElements = `
-    <h1>Home Page</h1>
+    <h1>Home Page with changes</h1>
     <p>This web site has all the information you need to become the best star wars fan ever<p>
   `;
 }
